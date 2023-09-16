335ef9de571ee5043cc9a6a2bc9c0663b4d1479a
Nikolay Danailov <frostblooded@yahoo.com>
2023-09-16 18:41:02 +0300

    Player can now take pile from desk

--- scripts/desk_interactable_area.gd
+++ scripts/desk_interactable_area.gd
@@ -1,5 +1,9 @@
 extends InteractableArea
 
+export var pile_scene: PackedScene = null
+
 
 func _on_interact():
-	print("Desk!")
+	print(pile_scene)
+	var player: Player = get_tree().root.get_node("Main").get_node("Player")
+	player.get_node("Inventory").add(pile_scene)
