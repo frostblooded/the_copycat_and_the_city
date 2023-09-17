3da756c1b115422f6c7de1a9643f51cb2dd5bb5d
Tanya Budinova <tanya.budinova@gmail.com>
2023-09-17 18:44:11 +0300

    Shredder script

--- scripts/pile.gd
+++ scripts/pile.gd
@@ -6,15 +6,15 @@ var is_failed: bool = false
 var desk: Node2D = null
 
 func set_as_copied():
-    is_copied = true
+	is_copied = true
 
-    if is_failed:
-        return
+	if is_failed:
+		return
 
-    $CopiedUI.visible = true
+	$CopiedUI.visible = true
 
 
 func set_as_failed():
-    is_failed = true
-    $CopiedUI.visible = false
-    $FailedUI.visible = true
+	is_failed = true
+	$CopiedUI.visible = false
+	$FailedUI.visible = true
