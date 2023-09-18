8659c483346ff5fc2ac1ae5f086b46377b7152b5
Nikolay Danailov <frostblooded@yahoo.com>
2023-09-18 19:13:15 +0300

    Make the printer stall sometimes

--- scripts/printer/printer_interactable_area.gd
+++ scripts/printer/printer_interactable_area.gd
@@ -7,14 +7,22 @@ signal pile_added(time)
 export var max_paper = 100
 export var current_paper = -1
 export var time_per_paper = 1.0
+export var stall_chance_per_frame: float = 0.001
+export var unsall_chance_per_interact: float = 0.3
 
-var timer = Timer.new()
+var timer: Timer = Timer.new()
 var is_ready = false
+var is_stalled: bool = false
 
 onready var inventory = get_node("Inventory")
 
 func is_empty():
 	return inventory.get_child_count() == 0
+
+
+func is_copying() -> bool:
+	return not timer.is_stopped()
+
 
 func _ready():
 	if current_paper == -1:
@@ -23,7 +31,23 @@ func _ready():
 	add_child(timer)
 	connect("pile_added", self, "_on_pile_added")
 
+
+func _process(delta):
+	if is_copying():
+		var val: float = randf()
+		var val2: float = stall_chance_per_frame
+		print("Start")
+		print(val)
+		print(val2)
+		if val < val2:
+			stall()
+
+
 func _on_interact():
+	if is_stalled:
+		if randf() < unsall_chance_per_interact:
+			unstall()
+
 	var player_inventory = Utils.get_player(get_tree()).get_inventory()
 	if is_empty():
 		if not player_inventory.has_items():
@@ -37,6 +61,21 @@ func _on_interact():
 			player_inventory.push_item(pile)
 	elif is_ready:
 		return_pile(player_inventory)
+
+
+func stall():
+	is_stalled = true
+	timer.paused = true
+	$StalledUI.visible = true
+	$Progress.visible = false
+
+
+func unstall():
+	is_stalled = false
+	timer.paused = false
+	$StalledUI.visible = false
+	$Progress.visible = true
+
 
 func return_pile(player_inventory):
 	var pile: Pile = inventory.get_child(0) as Pile
